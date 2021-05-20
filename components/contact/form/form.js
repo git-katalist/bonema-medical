a929fa515dac30853312af697e5999fc4108703e
tsots <1katalist@gmail.com>
2021-05-20 04:47:33 +0200

    email capabilty enabled

--- components/contact/form/form.js
+++ components/contact/form/form.js
@@ -1,12 +1,51 @@
+import { useState } from "react";
 import { Form } from "./styles";
 
 export default function ContactForm() {
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [number, setNumber] = useState("");
+    const [message, setMessage] = useState("");
+    const [submitted, setSubmitted] = useState(false);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log("Sending");
+        let data = {
+            name,
+            email,
+            number,
+            message,
+        };
+        fetch("/api/contact", {
+            method: "POST",
+            headers: {
+                Accept: "application/json, text/plain, */*",
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(data),
+        }).then((res) => {
+            console.log("Response received");
+            if (res.status === 200) {
+                console.log("Response succeeded!");
+                setSubmitted(true);
+                setName("");
+                setEmail("");
+                setNumber("");
+                setMessage("");
+            }
+        });
+    };
+
     return (
         <Form>
-            <form action="#">
+            <form>
                 <div className="input-group">
                     <label htmlFor="name">Name:</label>
                     <input
+                        onChange={(e) => {
+                            setName(e.target.value);
+                        }}
                         className="no-outline"
                         type="text"
                         name="name"
@@ -17,6 +56,9 @@ export default function ContactForm() {
                 <div className="input-group">
                     <label htmlFor="email">Email Address</label>
                     <input
+                        onChange={(e) => {
+                            setEmail(e.target.value);
+                        }}
                         className="no-outline"
                         type="email"
                         name="email"
@@ -25,11 +67,14 @@ export default function ContactForm() {
                     />
                 </div>
                 <div className="input-group">
-                    <label htmlFor="contact-number">Contact Number</label>
+                    <label htmlFor="number">Contact Number</label>
                     <input
+                        onChange={(e) => {
+                            setNumber(e.target.value);
+                        }}
                         className="no-outline"
                         type="tel"
-                        name="contact-number"
+                        name="number"
                         id=""
                         placeholder="Contact Number"
                     />
@@ -37,6 +82,9 @@ export default function ContactForm() {
                 <div className="input-group">
                     <label htmlFor="message">Message</label>
                     <textarea
+                        onChange={(e) => {
+                            setMessage(e.target.value);
+                        }}
                         className="no-outline"
                         name="message"
                         id=""
@@ -45,6 +93,12 @@ export default function ContactForm() {
                         placeholder="Your Message"
                     ></textarea>
                 </div>
+                <input
+                    type="submit"
+                    onClick={(e) => {
+                        handleSubmit(e);
+                    }}
+                />
             </form>
         </Form>
     );
