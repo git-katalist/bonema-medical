c978baa34948ef5cd6d6ed1cf80407e12ecb16e4
tsots <1katalist@gmail.com>
2021-05-20 15:24:39 +0200

    favicon changed

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -4,7 +4,7 @@ export default function (req, res) {
     let nodemailer = require("nodemailer");
     const transporter = nodemailer.createTransport({
         port: 465,
-        host: "smtp.gmail.com",
+        host: "mail.bonema.co.za",
         auth: {
             user: process.env.user,
             pass: process.env.password,
@@ -14,7 +14,7 @@ export default function (req, res) {
 
     const mailData = {
         from: process.env.user,
-        to: "mosalatsotetsi@gmail.com",
+        to: "bokang.mosala@bonema.co.za",
         subject: `Message From ${req.body.name}`,
         text: req.body.message + " | Sent from: " + req.body.email,
         html: `<h4>Sent from: ${req.body.name}</h4><h4>Contact Number: ${req.body.number}</h4>
